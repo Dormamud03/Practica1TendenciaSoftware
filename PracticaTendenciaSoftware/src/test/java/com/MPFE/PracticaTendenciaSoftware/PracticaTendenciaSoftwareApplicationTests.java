754fa0d470b56f08d343185e0b744c2c36e7d3dc
Dormamud03 <paulsanunga@gmail.com>
2021-06-15 14:49:05 -0500

    Proyecto

---
+++ PracticaTendenciaSoftware/src/test/java/com/MPFE/PracticaTendenciaSoftware/PracticaTendenciaSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MPFE.PracticaTendenciaSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaTendenciaSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
