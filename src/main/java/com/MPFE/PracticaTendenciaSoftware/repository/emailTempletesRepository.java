f7239e8fa588ff02073131c9645e8b1c7f2bce36
Dormamud03 <paulsanunga@gmail.com>
2021-06-16 20:41:58 -0500

    Fixed

--- src/main/java/com/MPFE/PracticaTendenciaSoftware/repository/emailTempletesRepository.java
+++ src/main/java/com/MPFE/PracticaTendenciaSoftware/repository/emailTempletesRepository.java
@@ -1,9 +1,9 @@
 package com.MPFE.PracticaTendenciaSoftware.repository;
 
-import com.MPFE.PracticaTendenciaSoftware.controller.EmailTemplatesController;
+import com.MPFE.PracticaTendenciaSoftware.model.EmailTemplates;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface emailTempletesRepository extends JpaRepository<EmailTemplatesController, Long> {
+public interface emailTempletesRepository extends JpaRepository<EmailTemplates, Long> {
 
 }
